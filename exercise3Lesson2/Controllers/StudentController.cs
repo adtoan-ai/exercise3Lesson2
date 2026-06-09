2afba9b758355c3a3521505009213ae2ba8d4b1e
adtoan-ai <adtoan2k4@gmail.com>
2026-06-09 21:31:43 +0700

    Add project files.

---
+++ exercise3Lesson2/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using exercise3Lesson2.Models;
+
+namespace exercise3Lesson2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Info()
+        {
+            ViewBag.Name = "Nguyễn Văn A";
+            ViewData["Age"] = 20;
+            Student sv1 = new Student { Major = "CNTT" };
+                return View(sv1);
+        }
+    }
+}
---
+++ exercise3Lesson2/Models/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace exercise3Lesson2.Models
+{
+    public class Student
+    {
+       public  string Major { get; set; }
+    }
+}
